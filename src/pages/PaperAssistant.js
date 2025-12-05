c9059939fe955242cf8ddf5fdd24b523fe7bef80
1329655015-ux <1329655015@qq.com>
2025-12-05 16:58:29 +0800

    Create PaperAssistant.js

---
+++ src/pages/PaperAssistant.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from '../components/Card';
+
+const PaperAssistant = () => {
+  return (
+    <div className="page">
+      <h1>论文助手</h1>
+      <Card title="功能说明">
+        <p>📝 论文助手功能正在开发中，即将上线！</p>
+        <p>AI辅助论文写作和编辑。</p>
+      </Card>
+    </div>
+  );
+};
+
+export default PaperAssistant;
