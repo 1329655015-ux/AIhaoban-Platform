83edc64d367764651b2622490ad5d8faea82fc93
1329655015-ux <1329655015@qq.com>
2025-12-05 16:57:29 +0800

    Update AISearch.js

--- src/pages/AISearch.js
+++ src/pages/AISearch.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import Card from '../components/Card';
 
+const AISearch = () => {
+  return (
+    <div className="page">
+      <h1>超级AI搜索</h1>
+      <Card title="功能说明">
+        <p>🔍 AI搜索功能正在开发中，即将上线！</p>
+        <p>智能搜索功能将提供更精准的搜索结果。</p>
+      </Card>
+    </div>
+  );
+};
+
+export default AISearch;
